2aca0aefa2d4422000abcbd7ebe6f4cc2f5b3e59
highjump <lixiang901112@163.com>
2019-01-09 23:05:12 +0800

    Added module for cooking tips page

--- src/pages/cooking-tip/cooking-tip.ts
+++ src/pages/cooking-tip/cooking-tip.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Tip} from "../../models/tip";
+import {FirebaseManager} from "../../helpers/firebase-manager";
 
 /**
  * Generated class for the CookingTipPage page.
@@ -16,20 +18,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 export class CookingTipPage {
 
-  tips: Array<String> = [];
+  keyword = '';
+
+  tipsAll: Array<Tip> = [];
+  tips: Array<Tip> = [];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams
   ) {
-    // init data
-    for (var i = 0; i < 10; i++) {
-      this.tips.push("aa");
-    }
+    // fetch tips
+    const dbRef = FirebaseManager.ref();
+
+    const query = dbRef.child(Tip.TABLE_NAME);
+    query.once('value')
+      .then((snapshot) => {
+        console.log(snapshot);
+
+        // clear
+        const aryTip = [];
+
+        snapshot.forEach(function(child) {
+          const t = new Tip(child);
+
+          aryTip.push(t);
+        });
+
+        this.tipsAll = aryTip;
+        this.tips = aryTip;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CookingTipPage');
   }
 
+  onChangeKeyword(event: UIEvent) {
+    this.tips = [];
+
+    // filter tips with keyword
+    for (const t of this.tipsAll) {
+      if (t.title.indexOf(this.keyword) >= 0) {
+        this.tips.push(t);
+        continue;
+      }
+
+      if (t.content.indexOf(this.keyword) >= 0) {
+        this.tips.push(t);
+      }
+    }
+  }
 }
